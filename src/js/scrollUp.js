2384c21629b8d9e5e6fd4c3413dada9cf60dce66
Niall <hello@niallcolman.co.uk>
2024-03-30 12:44:51 +0000

    first commit

---
+++ src/js/scrollUp.js
@@ -0,0 +1,7 @@
+const scrollUp = document.querySelector('.js-scroll-up');
+
+if (scrollUp) {
+    scrollUp.addEventListener('click', () => {
+        $('html, body').animate({ scrollTop: 0 }, 'fast');
+    });
+}
