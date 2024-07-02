81f0dff369492fefad9151c5c42a51640f7a843a
Niall <hello@niallcolman.co.uk>
2024-07-02 18:16:02 +0100

    Architectural wrap slider

--- src/js/architecturalWrapSlider.js
+++ src/js/architecturalWrapSlider.js
@@ -8,9 +8,9 @@ $(document).ready(function () {
         slidesToScroll: 1,
         arrows: true,
         prevArrow:
-            '<i class="fa-solid fa-chevron-left text-2xl text-light-green absolute -left-4 top-1/2 -translate-y-1/2 z-10"></i>',
+            '<i class="fa-solid fa-chevron-left text-2xl text-light-green absolute -left-3 sm:-left-4 top-1/2 -translate-y-1/2 z-10"></i>',
         nextArrow:
-            '<i class="fa-solid fa-chevron-right text-2xl text-light-green absolute -right-4 top-1/2 -translate-y-1/2 z-10"></i>',
+            '<i class="fa-solid fa-chevron-right text-2xl text-light-green absolute -right-3 sm:-right-4 top-1/2 -translate-y-1/2 z-10"></i>',
         responsive: [
             {
                 breakpoint: 1024,
@@ -22,6 +22,12 @@ $(document).ready(function () {
                 breakpoint: 768,
                 settings: {
                     slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
                 },
             },
         ],
